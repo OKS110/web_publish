07b2f493435b0befe45d0c04c9b8dd91d231e73d
oks110 <oks321098@gmail.com>
2024-11-06 18:07:52 +0900

    commit

--- javascript/1_variable.js
+++ javascript/1_variable.js
@@ -4,19 +4,23 @@
 
 // name 이라는 변수를 선언하고 홍길동 이름으로 초기화합니다.
 // age 변수를 선언하고, 20으로 초기화
+// address 변수를 선언하고 서울시로 초기화합니다.
 
 let name = "홍길동";
 let age = 20;
 const address = "서울시";
 
-// 백종원으로 바꿔주세요.
+// name을 백종원으로 바꿔주세요.
 name = "백종원";
-// let age = 40; 오류
-// age = "공유";
+// age를 30으로 바꿔주세요.
 age = 30;
+// let age = 40; 오류 - 같은 이름의 변수를 한 번 더 선언 불가
+// age = "공유"; - 가능. 타입이 string으로 변함
 
-// address = "부산시";
-// const 로 선언한 변수는 값 변경 불가
+
+// address = "부산시"; - 불가능
+// const 로 선언한 변수는 값 변경 불가 - 상수
+
 // address, name, age 변수의 값을 콘솔에 출력
 console.log(address);
 
