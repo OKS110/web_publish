dc42c3a4eaf9c6b5989ef9c0d8b56812254c44ae
oks110 <oks321098@gmail.com>
2024-11-26 16:16:33 +0900

    commit

--- javascript/13.json/kobisCommons.js
+++ javascript/13.json/kobisCommons.js
@@ -1,24 +1,41 @@
 /**
- * KOBIS 박스오피스 정보
+ * KOBIS 영화 정보 상세 호출 함수
  */
-export async function kobsBoxOffice(type, searchDt){
-    const key = `362298addeb513676d1cf80b15a870f5`;
-    const url = `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/search${type}BoxOfficeList.json?key=${key}&targetDt=${searchDt}`;
+export async function kobisMovieDetail(movieCd) {
+    const key = `1387ed83604df30a0c5d9dfdea0cba00`;
+    const url = `http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=${key}&movieCd=${movieCd}`;
     
-    let kobis = await fetch(url);
-    let jsonData = await kobis.json();
+    const movie = await fetch(url);
+    const jsonData = await movie.json();
+
+    return jsonData;
+}
+
+/**
+ * KOBIS 영화 리스트 호출 함수
+ */
+export async function kobisMovieList() {
+    const key = `1387ed83604df30a0c5d9dfdea0cba00`;
+    const curPage = 1;
+    const itemPerPage = 100;
+    const url = `http://kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=${key}&curPage=${curPage}&itemPerPage=${itemPerPage}`;
+    
+    const movieList = await fetch(url);
+    const jsonData = await movieList.json();
 
     return jsonData;
 }
 
 
 /**
- * KOBIS 영화 리스트 호출 함수
+ * KOBIS 박스오피스 정보 호출 함수
  */
-export async function kobsMovieList(){
-    const key = `362298addeb513676d1cf80b15a870f5`;
-    const url = `https://kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=${key}`;
+export async function kobisBoxOffice(type, searchDt) {
+    const key = `1387ed83604df30a0c5d9dfdea0cba00`;
+    const url = `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/search${type}BoxOfficeList.json?key=${key}&targetDt=${searchDt}`;
 
-    const movieList = await fetch(url);
-    const jsonData = await movieList.json();
+    const kobis = await fetch(url);
+    const jsonData = await kobis.json();
+
+    return jsonData;
 }
---
+++ javascript/15.kmdb/kmdb.js
@@ -0,0 +1,93 @@
+import {kmdb} from './commons.js';
+initForm();
+
+function initForm(){
+    let output = `
+        <h1>KMDB API</h1>
+        <div>
+            <select id ="type">
+                <option value="default">선택</option>
+                <option value="director">영화감독</option>
+                <option value="actor">영화배우</option>
+            </select>
+            <input type="text" id="type_value" placeholder="감독/배우">
+            <input type="text" id="title" placeholder="영화제목을 입력해주세요">
+            <button type="button" id="search">Search</button>
+        </div>
+        <div id="result"></div>
+    `;
+    document.querySelector("body").innerHTML = output;
+
+    //버튼 이벤트 추가
+    let button = document.querySelector('#search');
+    let type = document.querySelector('#type');
+    let typeValue = document.querySelector("#type_value");
+    let title = document.querySelector("#title");
+    button.addEventListener('click', function(e){
+        e.preventDefault();
+        if(type.value === "default"){
+            alert('타입을 선택해주세요');
+            type.focus();
+        }else if(typeValue.value === ''){
+            alert('감독/배우를 입력해주세요.');
+            typeValue.focus();
+        }else if(title.value === ''){
+            alert('영화제목을 입력해주세요');
+            title.focus();
+        }else{
+            searchMovieResult(type.value, typeValue.value, title.value); //KMDB API 연동 후 출력화면
+        }
+    });
+}//initForm()
+
+//KMDB API 연동 후 출력화면
+function searchMovieResult(type, value, title){
+    // console.log(type, value, title);
+    kmdb(type, value, title)
+    .then((result) => {
+        let count = result.TotalCount;
+        let output = ``;
+        if(count){
+            let info = result.Data[0].Result[0];
+            let postersArray = result.Data[0].Result[0].posters.split('|');
+            let stllsArray = result.Data[0].Result[0].stlls.split('|');
+            let directors = result.Data[0].Result[0].directors.director; //배열
+            let actors = result.Data[0].Result[0].actors.actor; //배열
+
+            let title = info.title.replaceAll('!HS', '').replaceAll('!HE', '');
+            const selectedActors = actors.slice(0, 10).map(actor => actor.actorNm).join(', ').replaceAll('!HS', '').replaceAll('!HE', '');
+
+            output += `
+                <div class="container">
+                <div class="container-content">
+                    <h3>${title}</h3>
+                    <h5>${info.titleEng} - ${info.prodYear}년</h5>
+                <hr>
+                    <p>[${info.type}]   ${info.rating}  ${info.nation}    ${info.runtime}분   ${info.repRlsDate}(개봉)</p>
+                    <p><span>제작사 : </span><span>${info.company}</span></p>
+                    <p><span>감독 : </span><span>${directors[0].directorNm}</span></p>
+                    <p><span>출연 : </span><span>${selectedActors}</span></p>
+                </div>
+                <div class="container-img"><img src="${postersArray[0]}" alt=""></div>
+            </div>
+            <div class="container-stills">
+            `;
+
+
+
+            stllsArray.forEach((still) => {
+              output += `<img src="${still}">`;
+              console.log(still);
+              
+            });
+            output += `</div>`;
+
+
+          
+        }else{
+            output += `<h5>검색하신 데이터가 존재하지 않습니다.</h5>`
+        }
+        document.querySelector('#result').innerHTML = output;
+    })
+    .catch(error => console.log(error));
+}
