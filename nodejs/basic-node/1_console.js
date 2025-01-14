fcff0113b2471bd4a4141a2748a017451e478423
oks110 <seokko12@naver.com>
2025-01-14 09:55:59 +0900

    commit

--- nodejs/basic-node/1_console.js
+++ nodejs/basic-node/1_console.js
@@ -1,13 +1,13 @@
 const string = 'abc';
 const number = 1;
-const boolean =  true;
+const boolean = true;
 const obj = {
-    "outside" : {"inside" : {"key" : "value"}}
-};
-const array = [1, 2, 3];
+    "outside" : { "inside" : { "key" : "value" } }
+}
+const array = [1,2,3];
 
-console.log(string);
-console.log(number);
-console.log(boolean);
-console.log(obj);
-console.log(array);
+console.log(string); //abc
+console.log(number); //1
+console.log(boolean); //true
+console.log(obj); //{ outside: { inside: { key: 'value' } } }
+console.log(array); //[ 1, 2, 3 ]
--- nodejs/basic-node/2_os.js
+++ nodejs/basic-node/2_os.js
@@ -1,10 +1,9 @@
-const os = require('os'); // require 내장객체는 NodeJs에서 제공하는 내장 모듈을 호출, import 동일
+const os = require('os');  // require 내장객체는 Nodejs에서 제공하는 내장 모듈을 호출, import 동일
 
-console.log(os.type());
-console.log(os.hostname());
-console.log(os.homedir());
+console.log(os.type()); //Windows_NT
+console.log(os.hostname()); //DESKTOP-L2T6EDQ
+console.log(os.homedir()); //C:\Users\tj
 // console.log(os.cpus());
-console.log(os.cpus().length);
-console.log(os.freemem());
-console.log(os.totalmem());
-
+console.log(os.cpus().length); //12
+console.log(os.freemem()); //18763038720
+console.log(os.totalmem()); //25709264896
