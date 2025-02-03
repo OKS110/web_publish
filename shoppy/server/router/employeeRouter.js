537e41dc919dfe06195fdc474f1dde9d0627645e
oks110 <seokko12@naver.com>
2025-02-03 16:19:44 +0900

    commit

--- shoppy/server/router/employeeRouter.js
+++ shoppy/server/router/employeeRouter.js
@@ -1,5 +1,6 @@
 import express from "express";
 import * as controller from "../controller/employeeController.js";
+
 const router = express.Router();
 
 router.get('/all', controller.getEmployeeAll);
